e56e6ec7ef8edfe1fcbd2d545843b79853a3183e
Jordan Rao <jordanrao@gmail.com>
2017-04-27 16:57:17 +0800

    added listing model

---
+++ db/migrate/20170427084723_create_listings.rb
@@ -0,0 +1,17 @@
+class CreateListings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :listings do |t|
+      t.string :home_type
+      t.string :listing_type
+      t.integer :guest
+      t.integer :bedroom
+      t.integer :bathroom
+      t.text :summary
+      t.string :address
+      t.integer :price
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
