db3b57469f2f2317b2ff2866a9fca37d04e92a15
Jordan Rao <jordanrao@gmail.com>
2017-04-27 16:25:39 +0800

    added omniauth facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,28 @@ class User < ApplicationRecord
 
   enum gender: [:undefined, :male, :female]
 
+  has_many :authentications, :dependent => :destroy
+
+  def password_optional?
+	    true
+	end
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+    create! do |u|
+      # user = User.create!(fullname: auth_hash["extra"]["raw_info"]["name"], email: auth_hash["extra"]["raw_info"]["email"], gender: auth_hash["extra"]["raw_info"]["gender"])
+      u.fullname = auth_hash["extra"]["raw_info"]["name"]
+      u.email = auth_hash["extra"]["raw_info"]["email"]
+      u.gender = auth_hash["extra"]["raw_info"]["gender"]
+      u.authentications << (authentication)
+      u.password = SecureRandom.hex(7)
+    end
+  end
+  
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
   root "pages#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
