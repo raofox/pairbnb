41b0beceae295bf9a4101b99f44ac97709cf28ab
Jordan Rao <jordanrao@gmail.com>
2017-04-28 12:32:27 +0800

    implement CRUD for listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
 
   belongs_to :user
 
+  enum property_type: [:house, :apartment, :bungalow]
+  enum room_type: [:shared_room, :private_room, :entire_place]
+
+
 end
--- db/migrate/20170427084723_create_listings.rb
+++ db/migrate/20170427084723_create_listings.rb
@@ -1,12 +1,13 @@
 class CreateListings < ActiveRecord::Migration[5.0]
   def change
     create_table :listings do |t|
-      t.string :home_type
-      t.string :listing_type
-      t.integer :guest
+      t.integer :property_type # => bungalow, apartment, house etc
+      t.integer :room_type # => shared, private, entire place etc.
+      t.integer :guest # => number of guest allowed
       t.integer :bedroom
       t.integer :bathroom
-      t.text :summary
+      t.string :title
+      t.string :summary
       t.string :address
       t.integer :price
       t.references :user
